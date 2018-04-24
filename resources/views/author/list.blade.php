575136ada3a6358544a17deaf62338e6b3076315
hotpea <hotpea@gmail.com>
2018-04-23 23:52:22 -0300

    view template

--- resources/views/author/list.blade.php
+++ resources/views/author/list.blade.php
@@ -1,7 +1,9 @@
-<html>
+@extends('index')
+
+@section('content')
     <body>
         <?php foreach($authors as $author) { ?>
             <p>Hello, <?php echo $author['firstName']; ?></p>
         <?php } ?>
     </body>
-</html>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>INDT - Avaliação</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark">
+            <!-- Navbar content -->
+        </nav>
+
+        {{--@section('sidebar')--}}
+            {{--This is the master sidebar.--}}
+        {{--@show--}}
+
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
